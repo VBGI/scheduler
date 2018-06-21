724bda315a85c4de86ae93e8a213821db3437836
Kislov Dmitry <kislov@easydan.com>
2018-06-21 14:34:48 +1000

    Show only present and future dates

--- scheduler/templatetags/scheduler_tags.py
+++ scheduler/templatetags/scheduler_tags.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from django import template
-
+from django.utils import timezone
 from ..forms import UserFillForm
 from ..models import ScheduleDates, ScheduleName
 
 register = template.Library()
 
 
+
 @register.inclusion_tag('scheduler_regform.html')
 def show_form(pk):
     error = ''
     try:
-        dates = ScheduleDates.objects.filter(name=pk)
+        dates = ScheduleDates.objects.filter(name=pk, date__gte=timezone.now())
         thenum = dates[0].name.maxnumber
     except ScheduleDates.DoesNotExist:
         error = u'Не определено ни одного расписания'
